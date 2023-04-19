16bdde5e50f24ac2ccb43d2b61ce7b5940078d1f
Mark Percival <m@mdp.im>
2023-04-19 17:47:05 +0000

    Minor eslint fix

--- client/src/utils/titleize.ts
+++ client/src/utils/titleize.ts
@@ -1,7 +1,7 @@
 export function titleize(slug: string) {
   const words = slug.split('-')
 
-  for (const i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     const word = words[i]
     words[i] = word.charAt(0).toUpperCase() + word.slice(1)
   }

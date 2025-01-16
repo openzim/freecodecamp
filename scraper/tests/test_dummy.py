16e7f10d82373846ca87f732131ed1fcfe0a19da
benoit74 <benoit74@users.noreply.github.com>
2025-01-16 12:39:00 +0000

    Remove dummy test

--- scraper/tests/test_dummy.py
+++
@@ -1,7 +0,0 @@
-from fcc2zim.constants import VERSION
-
-
-# dummy test just to check that everything is in place to add more tests / report
-# coverage
-def test_version():
-    assert VERSION and len(VERSION) > 0
